d4ad44818fbb0b098c25242e847d3c93ad2880c7
Noah Bennett <nbennett.320@gmail.com>
2019-09-07 15:35:41 -0400

    reacting

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,50 @@
 import React, { Component } from 'react'
 
-import './css/home.css'
+import './css/home2.css'
 
 import cloud1 from './img/home/clouds/nwcloud1sprites_364_1500.png'
 import cloud2 from './img/home/clouds/nwcloud2sprites_364_1500_2.png'
+import cloud3 from './img/home/clouds/nwcloud4sprites_462_2000.png'
+import musicText from './img/home/text/music_sprites2_cropped.png'
+import musicTextHover from './img/home/text/music_sprites_hover_500_164.png'
 
 class Home extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            musicText: false,
+            merchText: false,
+            goodiesText: false,
+        }
+    }
+
+    getMusicText = () => {
+        if (this.state.musicText === true) return musicTextHover
+        else return musicText
+    }
 
     render() {
 
         return (
 
-            <div className="home" style={styles.home}>
-
-                <div className="main" style={styles.main}>
-
-                    <div className="cloud" id="cloud1" style={styles.clouds.music}>
-                        <div id="cloudText" id="musicText" style={styles.text.music} />
+            <div className="home">
+                <div className="cloud_wrapper">
+                    <div className="clouds" id="cloud1" style={styles.clouds.music}>
+                        <div className="text" id="music_text" style={styles.text.music} />
                     </div>
 
-                    <div className="cloud" id="cloud2" style={styles.clouds.merch}>
-                        <div id="cloudText" id="merchText" style={styles.text.merch} />
+                    <div className="clouds" id="cloud2" style={styles.clouds.merch}>
+                        <div className="text" id="merch_text"
+                            onMouseEnter={() => {this.setState({ musicText: true })}}
+                        />
                     </div>
 
-                    <div className="cloud" id="cloud3" style={styles.clouds.goodies}>
-                        <div id="cloudText" id="goodiesText" style={styles.text.goodies} />
+                    <div className="clouds" id="cloud3" style={styles.clouds.goodies}>
+                        <div className="text" id="goodies_text"  />
                     </div>
-
                 </div>
-
             </div>
 
         )
@@ -38,39 +54,35 @@ class Home extends Component {
 }
 
 const styles = {
-    home: {
-        width: '100%',
-        background: '#fec0d3',
-    },
-    
-    main: {
-        width: '100%',
-        overflow: 'hidden',
-    },
+    // main: {
+    //     background: '#fec0d3',
+    //     backgroundSize: 'contain',
+    //     overflow: 'hidden',
+    //     width: '100%',
+    //     height: 'auto',
+    //     display: 'flex',
+    //     flexDirection: 'column',
+    //     alignItems: 'flex-start',
+    //     justifyContent: 'center',
+    // },
 
     clouds: {
         music: {
-            width: '500px',
-            height: '364px',
-            margin: '2% auto',
-            background: `url(${cloud1})`,
+            backgroundImage: `url(${cloud1})`,
         },
 
         merch: {
-            width: '500px',
-            height: '354px',
-            margin: '2% auto',
-            background: `url(${cloud2})`,
+            backgroundImage: `url(${cloud2})`,
         },
 
         goodies: {
-
+            backgroundImage: `url(${cloud3})`,
         }
     },
 
     text: {
         music: {
-
+            backgroundImage: `url(${musicText})`
         },
 
         merch: {
