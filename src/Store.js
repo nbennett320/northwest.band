88cb519ed2a20619f9ccd81ddd6f6dbe06c73fc1
Noah Bennett <nbennett.320@gmail.com>
2019-09-16 23:42:00 -0400

    add css for store

--- src/Store.js
+++ src/Store.js
@@ -7,7 +7,7 @@ class Store extends Component {
 
         0:{
             title: 'Northwest (PINK) Cursive Star T-Shirt',
-            img: './img/img_merch/IMG-2805_512.png',
+            img: `${require('./img/img_merch/IMG-2805_512.png')}`,
             altText: 'hot pink t-shirt with silver northwest cursive star logo',
             description: 'hot pink t-shirt with silver northwest cursive star logo',
             price: '12'
@@ -15,7 +15,7 @@ class Store extends Component {
 
         1:{
             title: 'Northwest (YELLOW) Cursive Star T-Shirt',
-            img: './img/img_merch/IMG-2801_512.png',
+            img: `${require('./img/img_merch/IMG-2801_512.png')}`,
             altText: 'happy yellow t-shirt with red northwest cursive star logo',
             description: 'happy yellow t-shirt with red northwest cursive star logo',
             price: '12'
@@ -23,8 +23,8 @@ class Store extends Component {
 
         2:{
             title: 'Northwest (RED) Cursive Star Crewneck',
-            img: './img/img_merch/IMG-2808_512.png',
-            altText: 'mega comfy crewneck with navy northwest cursive star logo',
+            img: `${require('./img/img_merch/IMG-2808_512.png')}`,
+            altText: 'comfy crewneck with navy northwest cursive star logo',
             description: 'mega comfy crewneck with navy northwest cursive star logo',
             price: '20'
         }
@@ -35,20 +35,21 @@ class Store extends Component {
 
         const items = this.items
 
-        
-
-        let catalog
+        let catalog = []
 
         for (let i = 0; i < Object.keys(items).length; i++) {
             console.log(items[i].title)
-            catalog += <ItemPreview 
-                title={items[i].title.toString()} 
+            catalog.push( <ItemPreview 
+                title={items[i].title} 
                 img={items[i].img} 
                 altText={items[i].altText} 
                 description={items[i].description} 
-            />
+                price={items[i].price}
+            /> )
 
         }
+
+        // catalog = <div> {catalog} </div>
 
         return catalog
 
@@ -62,7 +63,7 @@ class Store extends Component {
 
         return (
 
-            <div className="merch">
+            <div className="merch" style={styles.main}>
 
                 {this.mapCatalog()}
 
@@ -70,6 +71,20 @@ class Store extends Component {
 
         )
 
+    }
+
+}
+
+const styles = {
+
+    main: {
+        backgroundColor: '#e8e1b3',
+        height: 'calc(100vh - 8vh - 40px)',
+        paddingTop: 'calc(8vh + 40px)',
+        // marginTop: '40px',
+        display: 'flex',
+        flexDirection: 'row',
+        alignContent: 'flex-start',
     }
 
 }
