60fb1a8030b852dd570ca6b489d592bb6400a9fe
Noah Bennett <nbennett.320@gmail.com>
2019-10-11 10:53:31 -0400

    validate emaile

--- src/components/store/PlaceOrderPage.js
+++ src/components/store/PlaceOrderPage.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react'
 
+import validateEmail from '../../scripts/validateEmail'
+
 class PlaceOrderPage extends Component {
 
-    handleSubmit = () => {
+    constructor (props) {
+        super (props)
+
+        this.state = {
+            name: '',
+            email: '',
+            phone: '',
+            street: '',
+            zip: '',
+            region: '',
+        }
 
     }
 
-    handleChange = () => {
+    handleSubmit = () => {
+        
+    }
 
+    testValidation = () => {
+        console.log(this.state)
+        console.log(validateEmail(this.state.email))
+    }
+
+    handleChange = ev => {
+        ev.preventDefault()
+        this.setState({ [ev.target.name]: ev.target.value })
+    }
+
+    isBlank = input => {
+        if(input !== '') return false
+        else return true
+    }
+
+    displayWarning = warningString => {
+        console.log(warningString)
+        return <span className="warning-string" style={styles.warningString}> {warningString} </span>
     }
 
     render () {
@@ -20,53 +52,65 @@ class PlaceOrderPage extends Component {
 
                     <span style={styles.txt1}> Name </span>
                     <input
-                        type='name'
+                        type='text'
                         name='name'
                         style={styles.textBox}
+                        value={this.state.name}
                         onChange={this.handleChange}
                     />
 
                     <span style={styles.txt1}> Email </span>
                     <input
-                        type='email'
+                        type='text'
                         name='email'
                         style={styles.textBox}
+                        value={this.state.email}
                         onChange={this.handleChange}
                     />
+                    { () => { 
+                        if(
+                            this.isBlank(this.state.email) && 
+                            !validateEmail(this.state.email)
+                        ) return this.displayWarning("Please enter a valid email.") 
+                    }}
 
                     <span style={styles.txt1}> Phone Number </span>
                     <input
-                        type='phone'
+                        type='text'
                         name='phone'
                         style={styles.textBox}
+                        value={this.state.phone}
                         onChange={this.handleChange}
                     />
 
                     <span style={styles.txt1}> Street Address </span>
                     <input
-                        type='address'
-                        name='address'
+                        type='text'
+                        name='street'
                         style={styles.textBox}
+                        value={this.state.street}
                         onChange={this.handleChange}
                     />
 
                     <span style={styles.txt1}> Area Code </span>
                     <input
-                        type='zip'
+                        type='text'
                         name='zip'
                         style={styles.textBox}
+                        value={this.state.zip}
                         onChange={this.handleChange}
                     />
 
                     <span style={styles.txt1}> State </span>
                     <input
-                        type='state'
-                        name='state'
+                        type='text'
+                        name='region'
                         style={styles.textBox}
+                        value={this.state.region}
                         onChange={this.handleChange}
                     />
 
-                    <button style={styles.button}>Submit</button>
+                    <button style={styles.button} onMouseEnter={()=>this.testValidation()}>Submit</button>
 
                 </form>
 
@@ -121,7 +165,11 @@ const styles = {
         borderBottomRightRadius: '10px',
         borderTopLeftRadius: '10px',
         borderTopRightRadius: '10px',
-    }
+    },
+
+    warningString: {
+        color: 'red',
+    },
 
 }
 
---
+++ src/scripts/validateEmail.js
@@ -0,0 +1,65 @@
+const containsAtSymbol = emailString => {
+
+    for(let i = 0; i < emailString.length; i++) {
+
+        if(emailString.charAt(i) !== '@') continue
+        else return i
+
+    }
+
+    return false
+
+}
+
+const containsDotAfterAt = (atIndex, emailString) => {
+
+    for(let i = atIndex; i < emailString.length; i++) {
+
+        if(emailString.charAt(i) !== '.') continue
+        else return i
+
+    }
+
+    return false
+
+}
+
+const validNumOfCharsAfterFinalDot = (lastDotIndex, emailString) => {
+
+    if(emailString.length - lastDotIndex > 4) return false
+    else return true
+
+}
+
+const hasTwoDotsInRow = (emailString) => {
+
+    for(let i = emailString.length; i > 0; i--) {
+
+        if(
+            emailString.charAt(i) === '.' &&
+            emailString.charAt(i - 1) === '.'
+        ) return true
+
+    }
+
+    return false
+
+}
+
+const validateEmail = emailString => {
+
+    let atIndex = containsAtSymbol(emailString)
+    if(!atIndex) return false
+
+    let lastDotIndex = containsDotAfterAt(atIndex, emailString)
+    if(!lastDotIndex) return false
+
+    if(!validNumOfCharsAfterFinalDot(lastDotIndex, emailString)) return false
+
+    if(hasTwoDotsInRow(emailString)) return false
+
+    return true
+
+}
+
+export default validateEmail
