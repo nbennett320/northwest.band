1035a70483bbdaa2726b91db81e75e93f8a66dad
Noah Bennett <nbennett.320@gmail.com>
2020-08-31 18:47:42 -0400

    move lyrics to backend

--- src/views/music/Lyrics.js
+++ src/views/music/Lyrics.js
@@ -3,9 +3,16 @@ import { Helmet } from 'react-helmet'
 import LyricsHeader from './LyricsHeader'
 import LyricsBody from './LyricsBody'
 import Footer from '../../components/footer/Footer'
-import songs from '../../assets/data/Songs.json'
+import server from '../../server.config'
 
 class Lyrics extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      song: {}
+    }
+  }
+
   componentDidMount() {
     this.props.setHeaderLink('/music')
 
@@ -15,31 +22,30 @@ class Lyrics extends Component {
       this.props.setDestination({from: this.props.match.path})
       this.props.history.push('/blm')
     }
+
+    this.getSong()
   }
 
-  getSongFromKey = key => {
-    for(const i in Object.keys(songs)) {
-      const album = Object.keys(songs)[i]
-      for(const j in album) {
-        const song = songs[album][Object.keys(album)[j]]
-        if(makeKey(song["title"]) === key) {
-          return song
-        }
-      }
-    }
+  // get song data from backend
+  getSong = async () => {
+    const { match } = this.props
+    const { key } = match.params
+    const song = await fetch(`${server}/songs/${key}`,
+      {
+        method: 'GET',
+      }).then(res => res.json())
+    this.setState({ song })
   }
   
   render() {
+    const { song } = this.state
     const { match } = this.props
     const { key } = match.params
-    const song = this.getSongFromKey(key)
-    const url = require(`../../assets/img/lyrics/${song["album"].toLowerCase()}/${key}.jpg`)
     const helmet = makeHelmet(key, song)
-
-    return (
+    return Object.keys(song).length > 0 ? (
       <div style={{
           ...styles.main,
-          backgroundImage: `url('${url}')`
+          backgroundImage: `url('${require(`../../assets/img/lyrics/${song["album"].toLowerCase()}/${key}.jpg`)}')`
         }}
       >
         {helmet}
@@ -56,7 +62,9 @@ class Lyrics extends Component {
 
         <Footer location={this.props.location} />
       </div>
-    )
+    ) : <div style={styles.hidden}>
+        ( loading )
+      </div>
   }
 }
 
@@ -74,14 +82,14 @@ const styles = {
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
   },
-
   content: {
     margin: '0 auto',
     marginTop: 'calc(8vh + 40px)',
+  },
+  hidden: {
+    display: 'none'
   }
 }
-
-const makeKey = title => title.replace(/\s/g,'-').replace(/[.()]/g,'').toLowerCase()
 
 const makeHelmet = (key, song) => (
   <Helmet>
@@ -104,7 +112,7 @@ const makeHelmet = (key, song) => (
     <meta name="author" content="Noah Bennett" />
     <meta name="robots" content="index" />
     <meta name="url" content={`http://northwest.band/songs/${key}`} />
-    <title>northwest the band | {song.title.toLowerCase()} lyrics </title>
+    <title>northwest the band | lyrics </title>
     <meta name="description" content={`
       Lyrics for "${song.title}" by Northwest.
     `} />
