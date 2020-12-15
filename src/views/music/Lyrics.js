92e76f0ebd9ba3c87b06128b9bf1dc298597fdca
Noah Bennett <nbennett.320@gmail.com>
2020-12-15 01:04:02 -0500

    move lyrics back to frontend lol

--- src/views/music/Lyrics.js
+++ src/views/music/Lyrics.js
@@ -6,73 +6,58 @@ import LyricsBody from './LyricsBody'
 import Footer from '../../components/footer/Footer'
 import { LyricsHelmet as Helmet } from './Helmet'
 import server from '../../server.config'
+import songs from '../../assets/data/Songs.json'
 
-const useFetchLyrics = props => {
-  const [song, setSong] = React.useState({})
-  const [isLoading, setLoading] = React.useState(true)
-  const { key } = props.match.params
-  React.useEffect(() => {
-    const getSong = async () => {
-      try {
-        setLoading(true)
-        const res = await fetch(`${server}/songs/${key}`, { method: 'GET' })
-          .then(res => res.json())
-        setSong(res)
-      } catch(err) {
-        console.error("error fetching song lyrics", err)
-        setLoading(false)
+const getSongFromKey = key => {
+  let result
+  Object.keys(songs).forEach(i => {
+    songs[i].forEach(j => {
+      if(key === makeKey(j.title)) {
+        result = j
       }
-    }
-    if(Object.keys(song).length === 0) {
-      getSong()
-    } else {
-      setLoading(false)
-    }
-  }, [song, isLoading, key])
-  return [song, isLoading]
+    })
+  })
+  return result
 }
 
 const Lyrics = props => {
-  const [song, isLoading] = useFetchLyrics(props)
   const { match, location, device } = props
   const { key } = match.params
+  const song = getSongFromKey(key)
+  const backgroundImage = require(`../../assets/img/lyrics/${song["album"].replace(/\s+/g, '-').toLowerCase()}/${key}.jpg`)
   const hasShownBlmPanel = sessionStorage.getItem("hasShownBlmPanel")
   if(hasShownBlmPanel === "false") {
     props.setDestination({from: props.match.path})
     props.history.push('/blm')
   }
   props.setHeaderLink()
-  return isLoading 
-    ? (
-    <div style={styles.hidden}>
-    ( loading )
-    </div>
-    ) : (
-      <div style={{
-          ...styles.main,
-          backgroundImage: `url(${server}/assets/img/lyrics/${song["album"].replace(/\s+/g, '-').toLowerCase()}/${key}.jpg)`
-        }}
-      >
-        <Helmet 
-          song={song}
-          songKey={key}
+  return (
+    <div style={{
+        ...styles.main,
+        backgroundImage: `url(${backgroundImage})`
+      }}
+    >
+      <Helmet 
+        song={song}
+        songKey={key}
+      />
+      <div style={styles.content}>
+        <LyricsHeader 
+          title={song.title}
+          album={song.album}
+          date={song.date}
         />
-        <div style={styles.content}>
-          <LyricsHeader 
-            title={song.title}
-            album={song.album}
-            date={song.date}
-          />
-          <LyricsBody song={song} />
-        </div>
-        <Footer 
-          location={location} 
-          device={device}
-        />
+        <LyricsBody song={song} />
       </div>
-    )
-  
+      <Footer 
+        location={location} 
+        device={device}
+      />
+    </div>
+  )
 }
+
+const makeKey = title => title.replace(/\s/g,'-').replace(/[.()]/g,'').toLowerCase()
 
 const styles = {
   main: {
