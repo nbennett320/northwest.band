9b287e6851cc189d935379d344252e2de277cc05
Noah Bennett <nbennett.320@gmail.com>
2022-08-03 00:38:00 -0400

    consistent footer

--- northwest.band/pages/music.tsx
+++ northwest.band/pages/music.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/navbar/Navbar'
+import Footer from '../components/footer/Footer'
 import Album from './music/Album'
 import { getAllMusicData, MusicData } from '../lib/music'
 import styles from './styles.module.scss'
@@ -48,18 +49,7 @@ const Music = (props: Props) => {
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer />
     </div>
   )
 }
